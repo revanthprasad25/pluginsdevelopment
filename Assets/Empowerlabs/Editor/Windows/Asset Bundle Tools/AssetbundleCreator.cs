8d42c82e7de0b4fa17b86021888ba5c9f21e76f4
revanthprasad25 <revanth.prasad@empowerlabs.ooo>
2016-08-30 13:13:59 +0530

    Added button to create _AssetBundle folder explicity, in order to avoid code complexity

--- Assets/Empowerlabs/Editor/Windows/Asset Bundle Tools/AssetbundleCreator.cs
+++ Assets/Empowerlabs/Editor/Windows/Asset Bundle Tools/AssetbundleCreator.cs
@@ -19,6 +19,7 @@ namespace Empowerlabs.Editor
 		private BuildTarget m_buildTarget = BuildTarget.Android;
 		private BuildAssetBundleOptions m_options = BuildAssetBundleOptions.None;
 		private bool m_isAssetFolderCreated;
+		private int m_counter = 0;
 		#endregion
 
 	    #region MenuItems
@@ -43,8 +44,6 @@ namespace Empowerlabs.Editor
 
 			EditorGUILayout.BeginVertical ();
 
-			EditorGUILayout.LabelField ("Create a folder : \"Assets/Asset Bundle\"");
-				
 			GUILayout.Space (10);
 
 			m_options = (BuildAssetBundleOptions)EditorGUILayout.EnumPopup ("Bundle Options", m_options);
@@ -53,11 +52,26 @@ namespace Empowerlabs.Editor
 
 			m_buildTarget = (BuildTarget)EditorGUILayout.EnumPopup ("Build Target", m_buildTarget);
 
+			GUILayout.Space (20);
+
+			if (GUILayout.Button ("Click here to create Bundle folder"))
+			{
+				if (CheckForBundleFolder ()) {
+					if (EditorUtility.DisplayDialog ("Info", "Folder is already created, Do you want to start building", "Build", "Cancel")) {
+						if (EditorUtility.DisplayDialog ("Alert", "Are you sure , you to want to build Asset bundle \nOptions\t:" + m_options.ToString () + "\nTarget\t:" + m_buildTarget.ToString (), "Yes", "No")) {
+							Build ();
+						}
+					}
+				} else {
+					AssetDatabase.CreateFolder ("Assets", "_AssetBundle");
+				}
+			}
+
 			GUILayout.Space (10);
 
 			if (GUILayout.Button ("Build")) 
 			{
-				if (EditorUtility.DisplayDialog ("Dude!", "Are you sure , you to want to build Asset bundle \nOptions\t:" + m_options.ToString () + "\nTarget\t:" + m_buildTarget.ToString (), "Yep", "Nope"))
+				if (EditorUtility.DisplayDialog ("Alert", "Are you sure , you to want to build Asset bundle \nOptions\t:" + m_options.ToString () + "\nTarget\t:" + m_buildTarget.ToString (), "Yes", "No"))
 				{
 					Build ();
 				}
@@ -68,20 +82,35 @@ namespace Empowerlabs.Editor
 		#endregion
 
 		#region Methods
+
+		private bool CheckForBundleFolder()
+		{
+			string[] assetBundleFolder = AssetDatabase.GetSubFolders ("Assets");	//Gets all the subfolders in assets
+			for (int i = 0; i < assetBundleFolder.Length; i++)
+			{
+				//Asset Bundle folder is already created
+				if (assetBundleFolder [i].Contains ("_AssetBundle")) 
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+
 		private void Build()
 		{
-			if (BuildPipeline.BuildAssetBundles ("Assets/Asset Bundle", m_options, m_buildTarget)) 
+			if (BuildPipeline.BuildAssetBundles ("Assets/_AssetBundle", m_options, m_buildTarget)) 
 			{
 				CustomTools.LogMessage ("Asset Bundle Created");
 				string[] names = AssetDatabase.GetAllAssetBundleNames ();
-
 				for (int i = 0; i < names.Length; i++)
 					CustomTools.LogMessage (names [i]);
 			} 
 			else 
 			{
-				AssetDatabase.CreateFolder ("Assets", "Asset Bundle");
-				Build ();
+				EditorUtility.DisplayDialog ("Info", "Failed to create Assets Bundle \n 1. Atleast one asset should be under asset bundle \n 2. If you don't see \"_AssetBundle\" folder , please create under Assets", "OK");
 			}
 		}
 		#endregion
