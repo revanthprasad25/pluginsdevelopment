12f753f5ac83676d26d6bf929d13008ca5384485
revanthprasad25 <revanth.prasad@empowerlabs.ooo>
2016-08-30 17:37:29 +0530

    Folder specific inpector created

--- Assets/Empowerlabs/Editor/Inspectors/CustomFolderInspector.cs
+++ Assets/Empowerlabs/Editor/Inspectors/CustomFolderInspector.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.IO;
+
 
 [CustomEditor(typeof(DefaultAsset) , true)]
 public class CustomFolderInspector : Editor 
@@ -16,12 +18,12 @@ public class CustomFolderInspector : Editor
 	{
 		m_path = AssetDatabase.GetAssetPath (target);
 		m_skin = Resources.Load ("Editor Skins/CustomSkin", typeof(GUISkin)) as GUISkin;
-		GUI.enabled = true;
 	}
 		
 
 	public override void OnInspectorGUI ()
 	{
+		
 		if ( !AssetDatabase.IsValidFolder( m_path ) )
 		{
 			return;
@@ -33,14 +35,67 @@ public class CustomFolderInspector : Editor
 			DrawDefaultInspector ();
 			return;
 		}
+		GUI.enabled = true;
+
+		EditorGUILayout.BeginVertical (m_skin.GetStyle("window"));
+
+		string directoryName = Path.GetFileName (m_path);
+
+		EditorGUILayout.LabelField (directoryName, m_skin.GetStyle ("Header"));
+		EditorGUILayout.LabelField ("", m_skin.GetStyle ("tooltip"));
+		GUILayout.Space (1);
 
 		EditorGUILayout.BeginHorizontal ();
-	
+
+		if (GUILayout.Button ("Create Folder", m_skin.GetStyle ("button"))) 
+		{
+			EditorApplication.ExecuteMenuItem ("Assets/Create/Folder");
+		}
+
+		//Scenes Directory
+		if (directoryName.Equals ("Scenes")) 
+		{
+			if (GUILayout.Button ("Create New Scene", m_skin.GetStyle ("button"))) 
+			{
+				EditorApplication.ExecuteMenuItem ("Assets/Create/Scene");
+			}
+		}
+
+		//Scripts Directory
+		if (directoryName.Equals ("Scripts")) 
+		{
+			if (GUILayout.Button ("Create Script", m_skin.GetStyle ("button"))) 
+			{
+				EditorApplication.ExecuteMenuItem ("Assets/Create/C# Script");
+			}
+		}
+
+		//Prefab Directory
+		if (directoryName.Equals ("Prefabs")) 
+		{
+			if (GUILayout.Button ("Create Prefab", m_skin.GetStyle ("button"))) 
+			{
+				EditorApplication.ExecuteMenuItem ("Assets/Create/Prefab");
+			}
+		}
+
+		EditorGUILayout.EndHorizontal ();
+
+		EditorGUILayout.BeginHorizontal ();
+
+		if (GUILayout.Button ("Import", m_skin.GetStyle ("button"))) 
+		{
+			EditorApplication.ExecuteMenuItem ("Assets/Import New Asset...");
+		}
+
+		if (GUILayout.Button ("Export", m_skin.GetStyle ("button"))) 
+		{
+			EditorApplication.ExecuteMenuItem ("Assets/Export Package...");
+		}
+
+		EditorGUILayout.EndHorizontal ();
+
 		EditorGUILayout.EndVertical ();
 	}
-
-	private void ResourceHierarchy()
-	{
-		
-	}
 }
+
